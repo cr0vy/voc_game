c192f84bc052733b17dc2a065cef6421d8818991
Samuli Lehtonen <cr0xxy94@gmail.com>
2020-08-30 12:56:38 +0300

    Load words from json

--- game/game.py
+++ game/game.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import json
 import random
 
 
@@ -9,6 +10,8 @@ class Game:
         self.right_answers = 0
 
         self.words = []
+
+        self.load_words()
 
         self.start = 0
         self.end = len(self.words)
@@ -25,6 +28,19 @@ class Game:
 
     def get_words(self):
         return self.cur_word[0], self.cur_word[1]
+
+    def load_words(self, file: str = "lang/en_to_fi.json"):
+        with open(file, "r") as json_file:
+            data = json.load(json_file)
+
+            for word in data['words']:
+                source = word['source']
+                pron = word['pronounce']
+                target = word['target']
+                word_class = word['word_class']
+
+                word_dict = [source, pron, target, word_class]
+                self.words.append(word_dict)
 
     def set_next_word(self):
         self.cur_word = random.choice(self.words[self.start:self.end])
